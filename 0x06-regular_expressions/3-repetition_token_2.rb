2e732cc01bda18f000f80152d836df88f7732e94
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-08-01 11:06:47 +0000

    updated comment

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
 # This script takes a string as an argument and returns
 # all occurrences of ‘h’, ‘b’, ‘t’, or ‘n’ in the string.
+#
+# It matches one or more occurrences of ‘t’ using the + character
+# So the script matches any sequence of ‘h’, ‘b’, ‘t’, and one or more 'n’s.
 puts ARGV[0].scan(/hbt+n/).join
