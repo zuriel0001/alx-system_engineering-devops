deae7d3a852f6f93583861ee0dafe0bed1da7910
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-09-22 19:18:53 +0000

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file with the following details
 
 file { 'school':
-  path => '/tmp/school'
-  mode => '0744',	
-  owner => 'www-data',
-  group => 'www-data',
   content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
 }
