a6bdceb1463b30a3fd8926f5787705c0844c9319
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-09-24 15:59:18 +0000

    change client config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# Change the SSH Client config file
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
