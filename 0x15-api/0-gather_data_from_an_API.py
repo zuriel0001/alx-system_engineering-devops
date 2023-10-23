f3a5f84ee17f453e32d7e207951e6028381e0948
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-10-23 23:38:35 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", param={"userId": sys.argv[1]}).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [tsk.get("title") for tsk in todos
                  if tsk.get("completed") is True]
