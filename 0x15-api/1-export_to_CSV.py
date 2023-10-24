fc15e7652b0090d48508cae301bcad1cd6653160
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-10-24 00:06:11 +0000

    fixing error

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""
+Script to exports to-do list information for a given
+employee ID to a CSV format.
+"""
 import csv
 import requests
 import sys
@@ -14,5 +17,5 @@ if __name__ == "__main__":
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [user_id, username, task.get("completed"), task.get("title")]
-         ) for task in todos]
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
