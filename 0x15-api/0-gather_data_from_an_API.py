c56b970049b53694ca891f8bc6cf12f1d57dbc35
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-10-23 17:57:50 -0700

    fixes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Returns to-do list information for a given employee ID
+   The script uses particular REST API.
+"""
 import requests
 import sys
 
@@ -8,7 +11,9 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    completed = [task.get("title") for task in todos
+            if task.get("completed") is True]
+
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
