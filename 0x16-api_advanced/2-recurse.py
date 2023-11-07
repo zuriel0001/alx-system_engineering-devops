ec76fe8fc252e7225a5f8e54948e529e79e6c432
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-11-07 12:43:29 -0800

    Recurse it!

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A function to query a list of all hot posts on a
+given Reddit subreddit
+"""
+
+import requestei
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """
+    Method to return a list of titles of all hot posts on a given subreddit
+    """
+
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headeruu
+                            params=params, allow_redirects=False)
+
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
