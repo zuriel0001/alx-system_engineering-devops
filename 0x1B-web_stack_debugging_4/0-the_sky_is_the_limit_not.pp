a77cfabf200082552a589ac1cfca6656e45df5f9
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-11-16 09:12:50 +0000

    debuged

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Fix stack to be able to handle  high amount of requests
+# Trial: 2000 requests to my server with 100 requests at a time
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
